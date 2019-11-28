934285a3ebd3dbea0467b7e878cfcd3d6a55b5b2
turovod <turovodspb@gmail.com>
2019-11-28 23:55:24 +0300

    20.11.2019 Передвижение

--- Match3_U2019_2_0_/Match3_U2019_2_0/Assets/Scripts/Figure.cs
+++ Match3_U2019_2_0_/Match3_U2019_2_0/Assets/Scripts/Figure.cs
@@ -30,4 +30,23 @@ public class Figure : MonoBehaviour
         index = index_;
         //GetComponent<SpriteRenderer>().sprite = SM_Play.singlton.txFigure[index];
     }
+
+    public void OnMouseDown()
+    {
+        MoveFigure.singleton.ClickDown(this);
+        Debug.Log("down");
+    }
+
+    private void OnMouseUp()
+    {
+        MoveFigure.singleton.ClickUp(this);
+        Debug.Log("up");
+
+    }
+
+    public void SetCoord(int x_, int y_)
+    {
+        x = x_;
+        y = y_;
+    }
 }
---
+++ Match3_U2019_2_0_/Match3_U2019_2_0/Assets/Scripts/MoveFigure.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFigure : MonoBehaviour
+{
+    public Figure figureDown;
+    public Figure figureUp;
+    public int state;
+    public static MoveFigure singleton;
+
+    private void Awake()
+    {
+        singleton = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClickDown(Figure figure_)
+    {
+        state = 1;
+        figureDown = figure_;
+    }
+    public void ClickUp(Figure figure_)
+    {
+
+        figureUp = figure_;
+         Debug.Log("m1 "+ figureDown.x+" "+ figureDown.y+"   "+ figureUp.x+ " "+figureUp.y);
+       if (figureDown.x == figureUp.x)
+        {
+            Debug.Log("m2");
+
+            int _dy = figureDown.y - figureUp.y;
+            if (_dy == 1 || _dy == -1)
+            {
+                Debug.Log("m3");
+                Move();
+            }
+        }
+        if (figureDown.y == figureUp.y)
+        {
+            int _dx = figureDown.x - figureUp.x;
+            if (_dx == 1 || _dx == -1)
+            {
+                Move();
+            }
+        }
+    }
+
+    public void Move()
+    {
+        Debug.Log("move");
+
+        int _xd = figureDown.x;
+        int _yd = figureDown.y;
+        int _xu = figureUp.x;
+        int _yu = figureUp.y;
+
+        figureDown.transform.localPosition = new Vector3(_xu, _yu, 0);
+        figureUp.transform.localPosition = new Vector3(_xd, _yd, 0);
+
+        figureDown.SetCoord(_xu, _yu);
+        figureUp.SetCoord(_xd, _yd);
+
+    }
+
+
+}
